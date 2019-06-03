372d6baffa453e3d31a54b7118ce81e8825dc195
Alex <alxkom018@gmail.com>
2019-06-03 23:50:41 +0300

    Updates

--- precache-manifest.083318308c804cc3f00b49d671107157.js
+++ precache-manifest.083318308c804cc3f00b49d671107157.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "eda845e2069692303fbd2ddafb727013",
+    "revision": "08a39813fd5fd57e039754ff6d8a5d14",
     "url": "/kendoreacttest01/index.html"
   },
   {
@@ -8,15 +8,15 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/kendoreacttest01/static/css/2.c307ed68.chunk.css"
   },
   {
-    "revision": "3cad4a5f7ba185e5f941",
-    "url": "/kendoreacttest01/static/css/main.c86b7cb5.chunk.css"
+    "revision": "ed337b8bdefda9f0a4ca",
+    "url": "/kendoreacttest01/static/css/main.eacf0f6a.chunk.css"
   },
   {
     "revision": "8d0dd9ae9fff1061670a",
     "url": "/kendoreacttest01/static/js/2.5e836834.chunk.js"
   },
   {
-    "revision": "3cad4a5f7ba185e5f941",
+    "revision": "ed337b8bdefda9f0a4ca",
     "url": "/kendoreacttest01/static/js/main.19653674.chunk.js"
   },
   {
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/kendoreacttest01/precache-manifest.28191d25ae053ae92cae136e852f4393.js"
+  "/kendoreacttest01/precache-manifest.083318308c804cc3f00b49d671107157.js"
 );
 
 self.addEventListener('message', (event) => {
