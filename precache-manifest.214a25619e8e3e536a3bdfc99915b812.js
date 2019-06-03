1daee8012e236d0015390093fb1db96b54cff491
Alex <alxkom018@gmail.com>
2019-06-03 21:15:12 +0300

    Updates

---
+++ precache-manifest.214a25619e8e3e536a3bdfc99915b812.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5bc8cb5c8ee566dc0a7cc29b474c7f11",
+    "url": "/kendoreacttest01/index.html"
+  },
+  {
+    "revision": "bf384b1f170123249507",
+    "url": "/kendoreacttest01/static/css/2.052b7b7b.chunk.css"
+  },
+  {
+    "revision": "2a133b73c79d5705791f",
+    "url": "/kendoreacttest01/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "bf384b1f170123249507",
+    "url": "/kendoreacttest01/static/js/2.d96be384.chunk.js"
+  },
+  {
+    "revision": "2a133b73c79d5705791f",
+    "url": "/kendoreacttest01/static/js/main.f1cbb589.chunk.js"
+  },
+  {
+    "revision": "d799ad5534c3b181ea62",
+    "url": "/kendoreacttest01/static/js/runtime~main.3fd6899e.js"
+  }
+]);
